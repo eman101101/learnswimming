bee7734cbc6837d933fc8d4ca81f9ee979380d17
Evan Koumarelas <evankoumarelas@hotmail.com>
2023-05-20 11:33:15 -0400

    chaange

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Learn Swimming</span>,
   project: {
     link: 'https://github.com/eman101101/learnswimming',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/eman101101/learnswimming/blob/main',
   footer: {
     text: 'Created by Evan Koumarelas Using Nextra',
   },
